c889e56d04fbbe35f5685ce78570f4a610ea6d1b
cemetiere <kirodron@mail.ru>
2023-11-07 21:01:11 +0300

    some fixes

--- Lab3/task5.sql
+++ Lab3/task5.sql
@@ -3,8 +3,17 @@
 
 SELECT У.ГРУППА, AVG(date_part('year', AGE(Л.ДАТА_РОЖДЕНИЯ)))::integer AS "Средний возраст" FROM Н_УЧЕНИКИ У 
 JOIN Н_ЛЮДИ Л ON У.ЧЛВК_ИД = Л.ИД 
-GROUP BY У.ГРУППА HAVING AVG(date_part('year', AGE(Л.ДАТА_РОЖДЕНИЯ)))::integer = ( 
-    SELECT MAX(date_part('year', AGE(Л.ДАТА_РОЖДЕНИЯ)))::integer FROM Н_УЧЕНИКИ У 
-    JOIN Н_ЛЮДИ Л ON У.ЧЛВК_ИД = Л.ИД 
-    WHERE У.ГРУППА = '1100'
-); 
+GROUP BY У.ГРУППА, Л.ДАТА_СМЕРТИ HAVING
+CASE WHEN Л.ДАТА_СМЕРТИ < NOW() AND Л.ДАТА_СМЕРТИ iS NOT NULL THEN
+    AVG(date_part('year', AGE(Л.ДАТА_РОЖДЕНИЯ, Л.ДАТА_СМЕРТИ)))::integer = ( 
+        SELECT MIN(date_part('year', AGE(Л.ДАТА_РОЖДЕНИЯ, Л.ДАТА_РОЖДЕНИЯ)))::integer FROM Н_УЧЕНИКИ У 
+        JOIN Н_ЛЮДИ Л ON У.ЧЛВК_ИД = Л.ИД 
+        WHERE У.ГРУППА = '1100'
+    ) 
+ELSE AVG(date_part('year', AGE(Л.ДАТА_РОЖДЕНИЯ)))::integer = ( 
+        SELECT MIN(date_part('year', AGE(Л.ДАТА_РОЖДЕНИЯ)))::integer FROM Н_УЧЕНИКИ У 
+        JOIN Н_ЛЮДИ Л ON У.ЧЛВК_ИД = Л.ИД 
+        WHERE У.ГРУППА = '1100'
+    )
+END;
+
